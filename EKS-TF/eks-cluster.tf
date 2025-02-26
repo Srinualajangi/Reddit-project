2571a0a5e8a5c6584bade31b1d77b5de919123e0
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2025-02-26 21:26:51 +0530

    added features

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -18,6 +18,7 @@ resource "aws_s3_bucket" "tetris_bucket" {
     Name        = "TetrisBucket"
     Environment = var.environment
   }
+} 
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster-name
   role_arn = aws_iam_role.EKSClusterRole.arn
