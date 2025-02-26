fa8d2916170ffda5d66d26ab8def17a147b76e33
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2025-02-26 21:22:23 +0530

    added features

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,3 +1,23 @@
+resource "aws_s3_bucket" "tetris_bucket" {
+  bucket = var.bucket_name  # Use a variable to specify the bucket name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "TetrisBucket"
+    Environment = var.environment
+  }
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.cluster-name
   role_arn = aws_iam_role.EKSClusterRole.arn
@@ -10,3 +30,4 @@ resource "aws_eks_cluster" "eks-cluster" {
 
   depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
+
