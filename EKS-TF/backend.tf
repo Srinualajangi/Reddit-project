fbb4c821bdc6a2c6e3f066d5c507bee246df5efa
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2025-02-26 20:41:41 +0530

    added features

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,17 +1,17 @@
-#terraform {
-# backend "s3" {
-#    bucket         = "tetris-bucket"
-#    region         = "us-east-1"
-#    key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-#    dynamodb_table = "Lock-Files"
-#    encrypt        = true
-#    depends_on     = [aws_s3_bucket.tetris_bucket, aws_dynamodb_table.lock_files]
-#  }
-#  required_version = ">=0.13.0"
-#  required_providers {
-#    aws = {
-#      version = ">= 2.7.0"
-#      source  = "hashicorp/aws"
-#   }
-#  }
-#}
+terraform {
+ backend "s3" {
+    bucket         = "tetris-bucket"
+    region         = "us-east-1"
+    key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+    depends_on     = [aws_s3_bucket.tetris_bucket, aws_dynamodb_table.lock_files]
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+   }
+  }
+}
--- EKS-TF/backend_resources.tf
+++ EKS-TF/backend_resources.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
+#provider "aws" {
+#  region = "us-east-1"
+#}
 
 resource "aws_s3_bucket" "tetris_bucket" {
   bucket = "tetris-bucket"
