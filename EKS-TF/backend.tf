bd874b26967d44b5e8fd4c86658bf024d0c59b58
Srinu Alajangi --CNTR <Srinu_Alajangi@contractor.amat.com>
2025-02-26 18:48:05 +0530

    added features

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -5,7 +5,6 @@ terraform {
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
-    depends_on     = [aws_s3_bucket.tetris_bucket, aws_dynamodb_table.lock_files]
   }
   required_version = ">=0.13.0"
   required_providers {
